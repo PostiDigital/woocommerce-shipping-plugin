4270e1a62f21ed2cbe354f419750e0f9fd18b02c
Joose Vettenranta <joose.vettenranta@pakettikauppa.fi>
2020-04-01 14:56:31 +0300

    Update API Library

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,22 +20,11 @@ class ComposerStaticInitc0379f4a7e60de0a37707981e28ca684
         ),
     );
 
-    public static $prefixesPsr0 = array (
-        'C' => 
-        array (
-            'Composer\\Installers\\' => 
-            array (
-                0 => __DIR__ . '/..' . '/composer/installers/src',
-            ),
-        ),
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitc0379f4a7e60de0a37707981e28ca684::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitc0379f4a7e60de0a37707981e28ca684::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInitc0379f4a7e60de0a37707981e28ca684::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
