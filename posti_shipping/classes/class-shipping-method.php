bc053041d6a8278386bd20a10ec691cb045137bb
Joose Vettenranta <joose.vettenranta@pakettikauppa.fi>
2020-04-21 13:04:54 +0300

    Remove unnecessery constructor

--- posti_shipping/classes/class-shipping-method.php
+++ posti_shipping/classes/class-shipping-method.php
@@ -4,11 +4,6 @@ namespace Woo_Posti_Shipping;
 
 class Shipping_Method extends \Woo_Pakettikauppa_Core\Shipping_Method
 {
-    public function __construct($instance_id = 0)
-    {
-        parent::__construct($instance_id);
-    }
-
     public function get_core()
     {
         return \Woo_Posti_Shipping::get_instance();
