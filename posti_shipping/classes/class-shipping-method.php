ed7c55c763085598d3efff47b745175bc3c7e727
Joose Vettenranta <joose.vettenranta@pakettikauppa.fi>
2020-04-21 11:55:26 +0300

    Make mode always production and hide it

--- posti_shipping/classes/class-shipping-method.php
+++ posti_shipping/classes/class-shipping-method.php
@@ -3,7 +3,19 @@
 namespace Woo_Posti_Shipping;
 
 class Shipping_Method extends \Woo_Pakettikauppa_Core\Shipping_Method {
-public function get_core() {
+    public function get_core() {
       return \Woo_Posti_Shipping::get_instance();
+    }
+
+    protected function get_form_field_mode() {
+        return array(
+            'title'   => $this->get_core()->text->mode(),
+            'type'    => 'hidden',
+            'default' => 'production',
+            'options' => array(
+                'test'       => $this->get_core()->text->testing_environment(),
+                'production' => $this->get_core()->text->production_environment(),
+            ),
+        );
     }
 }
