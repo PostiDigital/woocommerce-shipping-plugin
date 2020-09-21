c2106bdbbd0fa0760fafcaa2dcb862dc6f65b1ab
Joose Vettenranta <joose.vettenranta@pakettikauppa.fi>
2020-09-21 11:29:56 +0300

    Updated readme file and version bump

--- posti-shipping.php
+++ posti-shipping.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Posti Shipping
- * Version: 1.0.1
+ * Version: 1.0.2
  * Plugin URI: https://github.com/PostiDigital/woocommerce-shipping-plugin
  * Description: Posti shipping service for WooCommerce.
  * Author: Posti
