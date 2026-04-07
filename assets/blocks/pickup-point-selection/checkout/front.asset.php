eb690c7ad3a92f4510a5a14b844b4d4bf1138fda
Marijus <markakk@gmail.com>
2026-04-07 10:58:40 +0300

    Remove PP when change method to home delivery

--- assets/blocks/pickup-point-selection/checkout/front.asset.php
+++ assets/blocks/pickup-point-selection/checkout/front.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '14967c44630494333144');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8cc49bd2e61122017765');
